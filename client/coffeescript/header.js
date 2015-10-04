c32d8cad9149e68fa6831df112e0e01b87c457ed
Sullivan Valaer <sullivanvalaer@gmail.com>
2015-10-04 14:15:51 -0700

    Updated routes and styled home page buttons

--- client/coffeescript/header.js
+++ client/coffeescript/header.js
@@ -1,14 +1,23 @@
-if (Meteor.isClient) {
-  Template.header.rendered = function() {
-    // Show sideNav
-  $('.button-collapse').sideNav();
-  // Hide sideNav
-  $('.button-collapse').sideNav('hide');
-  };
 
-  Template.header.helpers({
-    routeName: function () {
-      return FlowRouter.getRouteName();
-    }
-  });
-}
+Template.header.rendered = function() {
+  // Show sideNav
+$('.button-collapse').sideNav();
+// Hide sideNav
+$('.button-collapse').sideNav('hide');
+};
+
+Template.header.helpers({
+  routeName: function () {
+    return FlowRouter.getRouteName();
+  }
+});
+
+// Template.header.helpers({
+//   "active": function(parent) {
+//     var path_parts;
+//     path_parts = Router.current().route.url.split("/");
+//     if (_.contains(path_parts, parent)) {
+//       return "active";
+//     }
+//   }
+// });
--- client/index.coffee
+++
@@ -1,8 +0,0 @@
-if Meteor.isClient
-	Template.nav.helpers
-	  "active": (parent) ->
-	    path_parts = Router.current().route.url.split "/"
-	    if _.contains path_parts, parent
-	      return "active"
-	Template.nav.rendered = ->
-		$(".button-collapse").sideNav();
